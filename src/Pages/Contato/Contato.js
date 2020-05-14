b00f2a9e4053c7e937f85bba7a1a9d2ccaba18d4
hsmello <60452857+hsmello@users.noreply.github.com>
2020-05-14 14:37:39 +0100

    home + contact style

--- src/Pages/Contato/Contato.js
+++ src/Pages/Contato/Contato.js
@@ -4,17 +4,32 @@ import YoutubeVideo from '../Functionalities/YoutubeVideo'
 import GetPostData from '../Functionalities/GetPostData'
 
 export default function Contato() {
-    
+
     console.log(GetPostData.state.dataAPI)
     return (
         <div className="contato_all_page" >
-            
-            {/* <button onClick={() => GetPostData()} /> */}
-            
-            Contato page
-            <YoutubeVideo 
-                src="https://www.youtube.com/embed/gb_qeAY8Aq4"
-            />
+            <div className="title">
+                Onde você pode me encontrar?
+            </div>
+            <div className="subtitle">
+                Quer ver mais conteúdo?
+            </div>
+            <div className="text">
+                Para acompanhar todo o tipo de conteúdo que eu divulgo me siga no Instagram, YouTube e Twitter! Por essas redes o engajamento tem sido bacana e você pode acompanhar de perto o meu trabalho. 
+            </div>
+            <div className="subtitle">
+                Gostaria de fazer um depoimento?
+            </div>
+            <div className="text">
+                Cada vez mais tenho recebido feedback dos meus seguidores, o que me dá certeza de estar no caminho certo, me incentiva a continuar nesse trabalho e proporciona novos contatos. Estou disponível em qualquer rede social e também pelo meu e-mail carol@carol.carol
+            </div>
+            <div className="subtitle">
+                Está a procura de uma parceria?
+            </div>
+            <div className="text">
+                Caso você esteja interessado em uma parceria pode mandar mensagem através das minhas redes sociais ou enviar um e-mail para carol@carol.carol.  
+            </div>
+
         </div>
     )
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import './Home.css'
-import Carol from '../../Images/Carol_home.jpeg'
+import './Home.css';
+import Carol from '../../Images/Carol_home.jpeg';
+import WebIcon from '@material-ui/icons/Web';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import YouTubeIcon from '@material-ui/icons/YouTube';
 
 function Home() {
     return (
@@ -25,34 +28,43 @@ function Home() {
                 <div className="box_home_body" >
                     <div className="box_home_first">
                         <div className="box_home_first_title">
+                            <div className="title_icon">
+                                <InstagramIcon />
+                            </div>
                             Instagram
                         </div>
                         <p className="box_p">
                             No instagram você pode acompanhar bem de perto o meu dia-a-dia e dicas sobre o como valorizar quem você realmente é
                         </p>
-                        <a className="first button">
+                        <a className="first button"  href="/">
                             ACOMPANHE O MEU INSTA
                         </a>
                     </div>
                     <div className="box_home_second">
                         <div className="box_home_second_title">
+                            <div className="title_icon">
+                                <YouTubeIcon />
+                            </div>
                             YouTube
                         </div>
                         <p className="box_p">
                             No YouTube dá pra entrar em mais detalhes e ir a fundo em diversos tópicos! Lá o engajamento tem ficado bem divertido. Acompanhe a gente por lá também!
                         </p>
-                        <a className="second button">
+                        <a className="second button"  href="/">
                             SIGA O MEU CANAL
                         </a>
                     </div>
                     <div className="box_home_third">
                         <div className="box_home_third_title">
+                            <div className="title_icon">
+                                <WebIcon />
+                            </div>
                             Aqui!
                         </div>
                         <p className="box_p">
                             Selecione a categoria que quer ver sobre e fique por dentro do conteúdo! <br /> Para mais detalhes de contato, parceria ou depoimentos, clique no botão abaixo.
                         </p>
-                        <a className="third button">
+                        <a className="third button" href="/">
                             CONTATOS
                         </a>
                     </div>
