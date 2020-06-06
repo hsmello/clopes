44b29e067e61ed6925e7b94a09ef876640abf775
hsmello <60452857+hsmello@users.noreply.github.com>
2020-06-06 14:58:41 +0100

    jun push

---
+++ build/precache-manifest.513c084bfceaee226f0ac8cfcea2562e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4f04065ae8cd3150d4da2b05c7ac1b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bb52cf1d1f23361e7e2",
+    "url": "/static/css/2.9c8cab86.chunk.css"
+  },
+  {
+    "revision": "00549b5f47d120eafe57",
+    "url": "/static/css/main.ebd35d71.chunk.css"
+  },
+  {
+    "revision": "3bb52cf1d1f23361e7e2",
+    "url": "/static/js/2.23bc2e40.chunk.js"
+  },
+  {
+    "revision": "5727441246ffa49345a3574f984c2034",
+    "url": "/static/js/2.23bc2e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00549b5f47d120eafe57",
+    "url": "/static/js/main.3baaa804.chunk.js"
+  },
+  {
+    "revision": "23a3607c2949c2b59988",
+    "url": "/static/js/runtime-main.e331032a.js"
+  },
+  {
+    "revision": "f1180429d9ac2fa22179116df634fd08",
+    "url": "/static/media/Carol_Sobre.f1180429.jpeg"
+  },
+  {
+    "revision": "e49f4d41998fe13ae20567daa9e66025",
+    "url": "/static/media/Carol_home.e49f4d41.jpeg"
+  },
+  {
+    "revision": "eb60d7ac46a955ecf938f67dc21b5553",
+    "url": "/static/media/logo_nb.eb60d7ac.png"
+  },
+  {
+    "revision": "e3f0ff5f848cc3015022c36ceea28c63",
+    "url": "/static/media/logo_ondulindas_bg.e3f0ff5f.png"
+  }
+]);
